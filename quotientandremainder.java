ca46dc1f2d9130b53b8c9f1eac6d00ae9cf17424
MOHAMMADNIBRAZN <85448180+MOHAMMADNIBRAZN@users.noreply.github.com>
2021-06-12 16:45:26 +0530

    Add files via upload

---
+++ quotientandremainder.java
@@ -0,0 +1,15 @@
+package quotientandremainder;
+
+public class quotientandremainder {
+
+	public static void main(String[] args) {
+		
+		int number1 = 150 , number2 = 20;
+		int quotient = number1 / number2;
+		int remainder = number1 % number2;
+		System.out.println("quotient is:" + quotient);
+		System.out.println("remainder is:" + remainder);
+		
+	}
+
+}
